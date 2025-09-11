cec25c843c37d83432c469acfd002a6af6eb8764
CalebSFeng <caleb.s.feng@gmail.com>
2025-09-11 19:40:52 -0400

    feat: add registration

---
+++ Register.php
@@ -0,0 +1,59 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+    $firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$login = $inData["login"];
+    $password = $indata["password"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "ContactManager");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+        // Check if user login already exists
+		$checkLoginStmt = $conn->prepare("SELECT login from Users where login = ?");
+		$checkLoginStmt->bind_param("s", $login);
+        $checkLoginStmt->execute();
+        $checkLoginStmt->store_result();
+		
+		if ($checkLoginStmt->num_rows > 0) {
+        returnWithError($stmt->error);
+    } else {
+        // Prepare and bind
+        $stmt = $conn->prepare("INSERT INTO userdata (firstName, lastName, login, password) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+
+        if ($stmt->execute()) {
+            sendResultInfoAsJson("Account Created Successfully");
+        } else {
+            returnWithError($stmt->error); 
+        }
+
+        $stmt->close();
+        $conn->close();
+    }
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	
+?>
